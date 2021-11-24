b5cb53d28d30ae3f570acd7172a236e86f047120
mart974 <mohammad.bajalal546@gmail.com>
2021-11-24 21:25:52 +0330

    some refactore and using pyperclip instead of xclip

--- translateor.py
+++ translateor.py
@@ -1,14 +1,18 @@
-from subprocess import getoutput
-from googletrans import Translator
 from os import system
+from googletrans import Translator
+from pyperclip import paste
 
-text = getoutput("xclip -out -selection primary")
+text: str = paste()
 translator = Translator()
 translate = translator.translate(text, "fa")
 try:
     if len(text) > 50:
         system(f"notify-send --expire-time=100000 'translate' '{translate.text}'")
     else:
-        system(f"notify-send --expire-time=10000 'translate' '{text} : {translate.text}'")
+        system(
+            f"notify-send --expire-time=10000 'translate' '{text} : {translate.text}'"
+        )
 except Exception as error:
-    system("notify-send --expire-time=10000 'translate' 'translate failed try again'")
+    system(
+        f"notify-send --expire-time=10000 'translate' 'translate failed try again with this error {error}'"
+    )
